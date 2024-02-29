1b95354e2c7aee33cf88210d54bc440ec4f198a6
Mikeal <mwiemszra@gmail.com>
2024-02-29 19:16:32 +0100

    Done

--- WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Maps.MapControl.WPF;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
@@ -44,13 +45,21 @@ namespace WeatherApp
             Location pinLocation = map.ViewportPointToLocation(mousePosition);
             pin.Location = pinLocation;
 
-            HttpResponseMessage response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={pinLocation.Latitude}&lon={pinLocation.Longitude}&apiid={apiid}");
+            HttpResponseMessage response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={pinLocation.Latitude}&lon={pinLocation.Longitude}&appid={apiid}");
             string jsonString = await response.Content.ReadAsStringAsync();
+            WeatherInfo.root details = JsonConvert.DeserializeObject<WeatherInfo.root>(jsonString);
 
             if (response.IsSuccessStatusCode)
             {
-                WeatherInfo.root details = JsonConvert.DeserializeObject<WeatherInfo.root>(jsonString);
-                MessageBox.Show(details.weather[0].description.ToString());
+                // MessageBox.Show(details.weather[0].description.ToString());
+                double temp = details.main.temp - 273.15;
+                MessageBox.Show(
+                    $"Weather: {details.weather[0].main}.\n" +
+                    $"Description: {details.weather[0].description}.\n" +
+                    $"Temp: {(float)System.Math.Round(temp, 2)}°C\n" +
+                    $"Pressure: {details.main.pressure}hPa\n" +
+                    $"Humidity: {details.main.humidity}%"
+                );
             }
             else
             {
